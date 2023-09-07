d69f9afc4720f9642a83eabc96f524269da93656
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2023-09-07 19:48:16 +0600

    a

--- serial_send.py
+++ serial_send.py
@@ -1,12 +1,22 @@
 import time
 import serial
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout= 1)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
 time.sleep(0.1)
-
+i = 0
 
 
 while True:
-    send_data = "F100F100" + '\n'
+    i = i + 1
+    
+    #send_data = str(i%2) + 'F100F112' 
+
+    send_data = 'KF110F110G'
+    #send_data = send_data + '\n' 
     ser.write(bytes(send_data, 'utf-8'))
-    time.sleep(0.1)
-    print(send_data)
+    #ser.write(send_data.encode())
+    
+    packet = ser.readline()
+    print(packet)
+    
+    time.sleep(0.01)
+    
