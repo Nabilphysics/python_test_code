157563a74d3e4567f3a4ba29c8120167b7176869
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2023-09-20 16:11:26 +0600

    Update encoder_wrap_function.py

--- encoder_wrap_function.py
+++ encoder_wrap_function.py
@@ -1,34 +1,38 @@
 
-encoder_min = -5
-encoder_max = 5
-
-mult = 0 
-prev_encoder = 0
-curr_enc = 0 
-encoder_low_wrap = ((encoder_max - encoder_min) * 0.3 + encoder_min) 
-encoder_high_wrap = ((encoder_max - encoder_min) * 0.7 + encoder_min )
 
 
-def leftForwardEncoderCount(en):
-        global mult,prev_encoder,curr_enc,encoder_high_wrap, encoder_low_wrap
-        enc = en
-        if (enc < encoder_low_wrap and prev_encoder > encoder_high_wrap):
-            mult = mult + 1
-       
-        if (enc > encoder_high_wrap and prev_encoder < encoder_low_wrap):
-            mult = mult - 1
-            
-        curr_enc= 1.0 * (enc + mult * (encoder_max - encoder_min)) 
-        prev_encoder = enc
-        print(curr_enc)
+class EncoderWrap():
+    def __init__(self, encoder_min, encoder_max):
+        self.encoder_min = encoder_min
+        self.encoder_max = encoder_max
+        self.mult = 0 
+        self.prev_encoder = 0
+        self.curr_encoder = 0 
+        self.encoder_low_wrap = ((self.encoder_max - self.encoder_min) * 0.3 + self.encoder_min) 
+        self.encoder_high_wrap = ((self.encoder_max - self.encoder_min) * 0.7 + self.encoder_min )
+
+
+    def getEncTick(self, curr_value_from_mcu_enc):
+            #global mult,prev_encoder,curr_enc,encoder_high_wrap, encoder_low_wrap
+            encoder = curr_value_from_mcu_enc
+            if (encoder < self.encoder_low_wrap and self.prev_encoder > self.encoder_high_wrap):
+                self.mult = self.mult + 1
+           
+            if (encoder > self.encoder_high_wrap and self.prev_encoder < self.encoder_low_wrap):
+                self.mult = self.mult - 1
+                
+            self.curr_encoder = 1.0 * (encoder + self.mult * (self.encoder_max - self.encoder_min)) 
+            self.prev_encoder = encoder
+            return self.curr_encoder
 
 
 
 
 def main():
+    en = EncoderWrap(encoder_min=-5, encoder_max=5)
     while True:
         inp = int(input('Input Encoder: '))
-        leftForwardEncoderCount(inp)
+        print(en.getEncTick(inp))
 
 
 
